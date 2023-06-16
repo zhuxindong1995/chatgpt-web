5e2a8fdcbe710369ee4e0a79c832214f82148e13
zhuxindong1995 <127579454+zhuxindong1995@users.noreply.github.com>
2023-06-16 14:48:00 +0800

    Update index.ts

--- service/src/index.ts
+++ service/src/index.ts
@@ -57,5 +57,6 @@ router.post('/config', async (req, res) => {
 
 app.use('', router)
 app.use('/api', router)
+app.set('trust proxy', 1)
 
 app.listen(3002, () => globalThis.console.log('Server is running on port 3002'))
